8bd4539b15dd5e2cb606a32cfa11c39aded94ee1
tkddnr924 <tkddnr924@naver.com>
2020-12-04 02:31:18 +0900

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ const router = require("express-promise-router")();
 const api = require('./api')
 
 router.get("/", (req, res, next) => { res.json({sucess:true, message:"welcome index", data:{version:"1.0.0"}, action: "example.test" }) })
-router.post("/", (req, res, next) => { res.json({sucess:true, message:"welcome index", data:{version:"1.0.0", action: "example.test"}}) })
+router.post("/", (req, res, next) => { res.json({sucess:true, message:"welcome index", data:{version:"1.0.0"}, action: "example.test" }) })
 
 router.use("/api", api)
 module.exports = router
