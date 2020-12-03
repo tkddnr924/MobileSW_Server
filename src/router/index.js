5b09c2a0729cf149c005f431184df12a7e3c29d5
tkddnr924 <tkddnr924@naver.com>
2020-12-04 02:05:56 +0900

    add action fields

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 const router = require("express-promise-router")();
 const api = require('./api')
 
-router.get("/", (req, res, next) => { res.json({sucess:true, message:"welcome index", data:{version:"1.0.0"}}) })
-router.post("/", (req, res, next) => { res.json({sucess:true, message:"welcome index", data:{version:"1.0.0"}}) })
+router.get("/", (req, res, next) => { res.json({sucess:true, message:"welcome index", data:{version:"1.0.0"}, action: "example.test" }) })
+router.post("/", (req, res, next) => { res.json({sucess:true, message:"welcome index", data:{version:"1.0.0", action: "example.test"}}) })
 
 router.use("/api", api)
 module.exports = router
