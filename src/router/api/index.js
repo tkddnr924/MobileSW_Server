71890cc5e30f2311bba8ad913289de47a52ddb3b
tkddnr924 <tkddnr924@naver.com>
2020-12-04 00:06:34 +0900

    add action fields

--- src/router/api/index.js
+++ src/router/api/index.js
@@ -1,14 +1,15 @@
 const router = require("express-promise-router")();
 const AuthService = require("../../service/AuthService")
+const actions = require("../../service/androidAction");
 
 router.post('/join', async (req, res, next) => {
   const { email, password, username, age, sex } = req.body
   const service = new AuthService()
 
   const result = await service.create_user(email, password, username, age, sex)
-  if (!result.status) return res.json({ success: false, message: result.message })
+  if (!result.status) return res.json({ success: false, message: result.message, action: actions.AUTH_ERROR })
 
-  return res.json({ success: true, message: result.message })
+  return res.json({ success: true, message: result.message, action: actions.JOIN })
 })
 
 router.post('/login', async (req, res, next) => {
@@ -18,12 +19,12 @@ router.post('/login', async (req, res, next) => {
   const exp = 60 * 60 * 24 * 5 * 1000
   const result = await service.sign_in(token, exp)
 
-  if (!result.status) return res.json({ success: false, message: result.message })
+  if (!result.status) return res.json({ success: false, message: result.message, action: actions.AUTH_ERROR })
 
   const options = { maxAge: expiresIn, httpOnly: true, secure: true};
   res.cookie('JTA_LOGIN_TOKEN', result.data.token, options)
 
-  return res.json({ success: true, message: result.message, data: result.data })
+  return res.json({ success: true, message: result.message, data: result.data, action: actions.LOGIN })
 })
 
 router.post('/status', async (req, res, next) => {
@@ -31,20 +32,19 @@ router.post('/status', async (req, res, next) => {
   const service = new AuthService()
 
   const exp = 60 * 60 * 24 * 5 * 1000
-
   const result = await service.sign_check(token, exp)
 
-  if (!result.status) return res.json({ success: false, message: result.message })
+  if (!result.status) return res.json({ success: false, message: result.message, action: actions.AUTH_ERROR })
 
   const options = { maxAge: expiresIn, httpOnly: true, secure: true};
   res.cookie('JTA_LOGIN_TOKEN', result.data.token, options)
 
-  return res.json({ success: true, message: result.message, data: result.data })
+  return res.json({ success: true, message: result.message, data: result.data, action: actions.STATUS })
 })
 
 router.post('/logout', async (req, res, next) => {
   res.clearCookie('JTA_LOGIN_TOKEN');
-  return res.json({ success: true, message: '로그아웃!'})
+  return res.json({ success: true, message: '로그아웃!', action: actions.LOGOUT })
 })
 
 module.exports = router
---
+++ src/service/androidAction.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "LOGIN": "auth.login",
+  "JOIN": "auth.join",
+  "STATUS": "auth.status",
+  "LOGOUT": "auth.logout",
+  "AUTH_ERROR": "auth.error"
+}
